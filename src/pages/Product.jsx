541cf561c0e91e5bfb12e31d64351ee56fd2ccf3
DanielNevadoK <113299772+DanielNevadoK@users.noreply.github.com>
2023-04-11 14:32:34 +0200

    Hämtar API :)

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Product = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+    const [item, setItem] = useState([]);
+    
+    
+    async function fetchItem() {
+      const response = await fetch(
+        `https://product-api-production-7dbf.up.railway.app/products`
+      );
+      const data = await response.json();
+      setItem(data);
+    }
+    
+    function handleSearchChange(e) {
+      setSearchQuery(e.target.value);
+      fetchItem();
+    }
   return (
-    <div>Product</div>
+    <div className='movies'>
+          
+    </div>
   )
 }
 
