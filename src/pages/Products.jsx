ad1c686dbdff71d71f7a215ace6a916448998e4d
Emilio Bielanski <emilio.bielanski@outlook.com>
2023-04-11 14:50:37 +0200

    products css

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,12 +3,15 @@ import { useState, useEffect } from "react"
 
 const Products = () => {
   const [productList, setProductList] = useState([]);
-  async function fetchProducts() {
-    const response = await fetch("https://product-api-production-7dbf.up.railway.app/products")
-    const productList = await response.json();
-    setProductList(productList);
-  }
-  fetchProducts();
+ 
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("https://product-api-production-7dbf.up.railway.app/products")
+      const productList = await response.json();
+      setProductList(productList);
+    }
+    fetchProducts();
+  }, []);
   return (
     <div className='pagediv'>
     <ul className='productUL'>
@@ -16,7 +19,8 @@ const Products = () => {
       <li className='productListItem' key={products._id}> 
       <img src="../../imgs/0916.apnews.steelydanlps_0.jpg" alt="" />
       <br />
-      <a href="#">{products.title} {products.price}kr</a>
+      <p>{products.title} {products.price}kr</p>
+      <a href="#">Read More</a>
       <br />
       <button>Add To Cart</button>
       </li>
