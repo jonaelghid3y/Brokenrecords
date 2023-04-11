457377ff46199aec5fe9f06851c2d3079d2a38b3
jonaelghid3y <jonaelghidey@hotmail.com>
2023-04-11 22:00:55 +0200

    förstadagen

--- src/pages/admin/ManageProducts.jsx
+++ src/pages/admin/ManageProducts.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from "react"
+import Product from '../Product';
 
 
 
@@ -7,8 +8,8 @@ import { useState, useEffect } from "react"
 const ManageProducts = () => {
 
   const [productList, setProductList] = useState([]);
-  
-  
+
+
   useEffect(() => {
     async function fetchProducts() {
       const response = await fetch("https://product-api-production-7dbf.up.railway.app/products")
@@ -19,30 +20,52 @@ const ManageProducts = () => {
     fetchProducts();
   }, []);
 
-  
+
   return (
     <div className="pagediv">
+      <h1> Manageproduct</h1>
+      <table id="table">
+            
+      
+            
+            <tr>
 
-    <h1> Manageproduct</h1>
-    <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table>
 
-  </div>
+                <th>Titel</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>stock</th>
+                <th>Hantera</th>
+                
+                
+            </tr>
+
+      
+
+
+      {productList.map((products) =>
+      <tr>
+      <td>{products.title}</td>
+      <td>{products.description}</td>
+      <td>{products.price}</td>
+      <td>{products.stock}</td>
+      
+      
+      <td> <button class="uppdatebtn" href="">Ändra</button><button class="deletebtn" href="#" /* data-id="${blog._id}"*/>Ta bort</button></td>
+    </tr>
+       
+        
+
+
+        
+      )}
+        
+        
+        </table>
+      
+      
+
+    </div>
   )
 }
 
