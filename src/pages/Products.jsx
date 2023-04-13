dad903a7f14517722129a7667f88bf0cf61b8b60
Emilio Bielanski <emilio.bielanski@outlook.com>
2023-04-13 11:26:59 +0200

    createProducts Skeleton done

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -22,12 +22,16 @@ const Products = () => {
   fetchProducts();
   return (
 
+ // Genre : {products.category}
+
+
     <div className='pagediv'>
     <ul className='productUL'>
       {productList.map((products) =>
       <li className='productListItem' key={products._id}> 
-      <img src="../../imgs/0916.apnews.steelydanlps_0.jpg" alt="" />
-      <h5>{products.title} </h5>
+      <img className='bigListImage' src={products.image} alt="" />
+      <h4>{products.title} </h4>
+      <h5>{products.description}  {products.releaseyear}</h5>
       <p>{products.price}kr</p>
       <Link to="/Product" >  Read More </Link>
       <button><img src="../../imgs/352007_add_cart_shopping_icon.svg" alt="" className='shoppingCartIcon' /></button>
