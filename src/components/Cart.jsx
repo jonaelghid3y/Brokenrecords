5049de2a8674ec76697f9c2f88335dbaded76ec6
klasnasman <klas.nasman@gmail.com>
2023-04-13 17:20:47 +0200

    first commit checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+// Cart.jsx
 
-const Cart = () => {
+import React from 'react';
+
+const Cart = ({ cart }) => {
   return (
-    <div>Cart</div>
+
+    <div>
+      <h3>Cart:</h3>
+      <ul>
+        {cart && cart.map((product) =>
+          <li key={product._id}>{product.title} ({product.price}kr)</li>
+        )}
+      </ul>
+    </div>
+
   )
 }
 
-export default Cart
+export default Cart;
