ff1063d1cfccb3b88e6aaf46a92c4757115e701e
klasnasman <klas.nasman@gmail.com>
2023-04-13 15:02:24 +0200

    tar bort en fetch den går 2 ggr

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Products = () => {
   const [productList, setProductList] = useState([]);
- 
+
   useEffect(() => {
     async function fetchProducts() {
       const response = await fetch("https://product-api-production-7dbf.up.railway.app/products")
@@ -13,28 +13,22 @@ const Products = () => {
     }
     fetchProducts();
   }, []);
- 
-  async function fetchProducts() {
-    const response = await fetch("https://product-api-production-7dbf.up.railway.app/products")
-    const productList = await response.json();
-    setProductList(productList);
-  }
-  fetchProducts();
+
   return (
 
     <div className='pagediv'>
-    <ul className='productUL'>
-      {productList.map((products) =>
-      <li className='productListItem' key={products._id}> 
-      <img className='bigListImage' src={products.image} alt="" />
-      <h4>{products.title} </h4>
-      <h5>{products.description}  {products.releaseyear}</h5>
-      <p>{products.price}kr</p>
-      <Link to={`/Product/${products._id}`} productId={products._id}>Read More</Link>    
-       <button><img src="../../imgs/352007_add_cart_shopping_icon.svg" alt="" className='shoppingCartIcon' /></button>
-      </li>
-      )}
-    </ul>
+      <ul className='productUL'>
+        {productList.map((products) =>
+          <li className='productListItem' key={products._id}>
+            <img className='bigListImage' src={products.image} alt="" />
+            <h4>{products.title} </h4>
+            <h5>{products.description}  {products.releaseyear}</h5>
+            <p>{products.price}kr</p>
+            <Link to={`/Product/${products._id}`} productId={products._id}>Read More</Link>
+            <button><img src="../../imgs/352007_add_cart_shopping_icon.svg" alt="" className='shoppingCartIcon' /></button>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
