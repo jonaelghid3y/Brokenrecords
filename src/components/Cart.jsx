70178f9004a969de84f0b1faff56420b0e16753a
klasnasman <klas.nasman@gmail.com>
2023-04-14 09:52:58 +0200

    cart price done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-
 const Cart = ({ cart }) => {
+  const totalPrice = cart.reduce((total, product) => total + product.price, 0);
+
   return (
     <div className='cart__container'>
       <ul className='cart__ul'>
         {cart && cart.map((product) =>
-          <li className='cart__li' key={product._id}><img className='cart__img' src={product.image} alt="" />
+          <li className='cart__li' key={product._id}>
+            <img className='cart__img' src={product.image} alt="" />
             <h4>{product.title}</h4>
             <h5>{product.description} {product.releaseyear}</h5>
             <p>{product.price}kr</p>
           </li>
         )}
       </ul>
+      <p>Total price: {totalPrice}kr</p>
     </div>
-
   )
 }
 
